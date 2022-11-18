e52e488707c135d427a040f8745b8cc6535eea32
Yohane <yohane910@gmail.com>
2022-11-18 21:05:08 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+import os
+
+yohaneUsername = "Yohane_0w0"
+yohanePw = "Yohane910"
+options = Options()
+# driver = webdriver.Chrome(options=options)
+
+# try:
+#     driver.find_element_by_xpath("")
+
+def sign_in():
+    driver = webdriver.Chrome()
+    url = 'https://www.youtube.com/?gl=TW&hl=zh-tw'
+    driver.get(url)
+    # driver.maximize_window()
+    time.sleep(5)
+    usernameElement = driver.find_element(By.XPATH, '//*[@id="email"]')
+    pwElement = driver.find_element(By.XPATH, '//*[@id="password"]')
+
+# confirmElement.click()
+
+# sign_in()
