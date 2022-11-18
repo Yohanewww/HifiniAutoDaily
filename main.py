beb8fcc29246aeb690a1fe5fb43c3da913273906
Yohane <yohane910@gmail.com>
2022-11-18 23:36:44 +0800

    GGG Captcha

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 import os
+import random
 
-yohaneUsername = "Yohane_0w0"
-yohanePw = "Yohane910"
 options = Options()
 # driver = webdriver.Chrome(options=options)
 
@@ -15,13 +14,25 @@ options = Options()
 
 def sign_in():
     driver = webdriver.Chrome()
-    url = 'https://www.youtube.com/?gl=TW&hl=zh-tw'
+    url = 'https://hifini.com/user-login.htm'
     driver.get(url)
     # driver.maximize_window()
     time.sleep(5)
     usernameElement = driver.find_element(By.XPATH, '//*[@id="email"]')
+    usernameElement.click()
+    time.sleep(random.uniform(1,3))
+    usernameElement.send_keys('')
     pwElement = driver.find_element(By.XPATH, '//*[@id="password"]')
+    pwElement.click()
+    time.sleep(random.uniform(1,3))
+    pwElement.send_keys('')
+    captchaElement = driver.find_element(By.XPATH, '//*[@id="captcha"]')
+    captchaElement.click()
+    time.sleep(10)
+
+
+
 
 # confirmElement.click()
 
-# sign_in()
+sign_in()
